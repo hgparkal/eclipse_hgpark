37a578785cdacf161bdab1cc724f51184f01fb5f
Yun <yds927848@naver.com>
2020-11-03 09:58:29 +0900

    create a department

--- Helloworld/src/First/Helloworld.java
+++ Helloworld/src/First/Helloworld.java
@@ -5,6 +5,8 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Park, Hyuk Gyu");
+		System.out.println("Computer SW engineering");
+	
 	}
 
 }
